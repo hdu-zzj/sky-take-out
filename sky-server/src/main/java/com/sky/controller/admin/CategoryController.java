74eb9f48c1babcb611133e917c25b20681fd872b
zzj <1535776587@qq.com>
2023-07-20 22:36:29 +0800

    新增分类

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,5 +1,36 @@
 package com.sky.controller.admin;
 
+import com.sky.dto.CategoryDTO;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api(tags = "分类操作")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     *
+     * @param categoryDTO
+     * @return
+     */
+    @ApiOperation(value = "新增分类")
+    @PostMapping
+    public Result save(@RequestBody CategoryDTO categoryDTO) {
+        log.info("新增分类：{}", categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+
 }
---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.time.LocalDateTime;
+
+@Mapper
+public interface CategoryMapper {
+
+    @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
+            "values " +
+            "(#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void insert(Category category);
+
+
+}
